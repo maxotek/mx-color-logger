3f9e1b319f6d53d7e591b2795c9d4a03cb2a3cf5
partho <partho@maxotek.com>
2018-03-01 22:34:52 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var clc = require("cli-color");
+
+var mapping = {
+    debug: clc.blue,
+    log: clc.blue,
+    warn: clc.yellow,
+    error: clc.red
+};
+
+module.exports = function () {
+    return {
+        init: function () {
+            ["log", "warn", "error"].forEach(function (method) {
+                var oldMethod = console[method].bind(console);
+                console[method] = function () {
+                    oldMethod.apply(
+                        console,
+                        [mapping[method]()]
+                            .concat(arguments)
+                    );
+                };
+            });
+        }
+    }
+}
